35a7026732ab0a57268bfa85ffd33d0fb3674444
BarZamsky <barzamsky1@gmail.com>
2018-09-19 21:52:01 +0300

    Send messages to only people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -54,12 +54,6 @@ socket.on('newMessage', function (message) {
 
   jQuery('#messages').append(html);
   scrollToBottom();
-
-  // console.log('NewMessage', message);
-  // var li = jQuery('<li></li>');
-  // li.text(`${message.from} ${formattedTime}: ${message.text}`);
-  //
-  // jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -88,7 +82,6 @@ jQuery('#message-form').on('submit', function (e) {
 
   var messageTextBox = jQuery('[name=message]');
   socket.emit('createMessage', {
-    from: 'User',
     text: messageTextBox.val()
   }, function () {
     messageTextBox.val('');
